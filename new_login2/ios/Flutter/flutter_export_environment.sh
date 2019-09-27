84d806dce36bc11fadc503a7f8b94557f7c5821f
yoghesh <yoghesh.d@gmail.com>
2019-09-28 02:05:47 +0530

    changes in code

--- new_login2/ios/Flutter/flutter_export_environment.sh
+++ new_login2/ios/Flutter/flutter_export_environment.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\flutter\flutter"
-export "FLUTTER_APPLICATION_PATH=E:\AndroidStudioProjects\flutter\dummy\new_login2"
+export "FLUTTER_ROOT=F:\SDKs\flutter_windows_v1.0.0-stable\flutter"
+export "FLUTTER_APPLICATION_PATH=F:\Dithok\Intern\devOps\new_login2\new_login2"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build\ios"
-export "FLUTTER_FRAMEWORK_DIR=C:\flutter\flutter\bin\cache\artifacts\engine\ios"
+export "FLUTTER_FRAMEWORK_DIR=F:\SDKs\flutter_windows_v1.0.0-stable\flutter\bin\cache\artifacts\engine\ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
